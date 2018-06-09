33859865b820243571f17fe6271498a6e0cbeafe
LucianaOliveira21 <luh_fbgo@hotmail.com>
2018-06-09 20:21:50 -0300

    Testando o salvar medicamentos

---
+++ projeto/tests/Feature/MedicamentoTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MedicamentoTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreateMedicamento()
+    {
+        $data = [
+            'nome' => 'Topiramato',
+            'fabricante' => 'Germed',
+            'qtd' => 10,
+            'peso' => 0.1,
+            'indicacoes' => 'para tratamento de epilepsia',
+            'contraindicacoes' => 'não deve ser usado durante a gravidez',
+            'reacoes' => 'nenhuma',
+        ];
+      
+        $this
+            ->post(route('medicamentos.store'), $data)
+            ->assertStatus(302)
+            ->assertRedirect(route('medicamentos.index'));
+    }
+}
